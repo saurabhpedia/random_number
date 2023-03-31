28ca4892e17298f08e336d23c1f4bc0e74c9aefc
lmsworld <--add>
2023-03-31 11:52:09 +0200

    1.0.2

---
+++ index.js
@@ -0,0 +1,6 @@
+module.exports = function generateRandomId() {
+  const timestamp = Date.now().toString(); // Get current timestamp as string
+  const random = Math.random().toString().slice(2, 8); // Get random number between 0 and 1, convert to string, and take the 2nd to 8th characters
+
+  return `${timestamp}${random}`; // Combine timestamp and random number to create ID
+}
